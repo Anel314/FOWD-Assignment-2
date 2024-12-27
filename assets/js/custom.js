4ec81b821526e3dc8933db5acea07ab9623fbcec
Anel314 <anel.brcaninovic@stu.ibu.edu.ba>
2024-12-27 23:21:53 +0100

    Added a homepage, some cool styles, dynamic navbar buttons, almost finished wooo, just the hard part left for now. I'm happayyyyy

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,40 @@ $(document).ready(function () {
   $("main#spapp > section").height($(document).height() - 60);
 
   var app = $.spapp({ pageNotFound: "error_404" }); // initialize
+  const navLinks = {
+    home: [
+      document.getElementById("hbtn"),
+      document.getElementById("hbtn-footer"),
+    ],
+    questions: [
+      document.getElementById("qbtn"),
+      document.getElementById("qbtn-footer"),
+    ],
+    booking: [
+      document.getElementById("bbtn"),
+      document.getElementById("bbtn-footer"),
+    ],
+    comments: [
+      document.getElementById("cbtn"),
+      document.getElementById("cbtn-footer"),
+    ],
+    portfolio: [
+      document.getElementById("pbtn"),
+      document.getElementById("pbtn-footer"),
+    ],
+  };
+
+  function updateActiveLink(sectionId) {
+    Object.keys(navLinks).forEach((key) => {
+      navLinks[key].forEach((link) => {
+        if (key === sectionId) {
+          link.classList.add("active");
+        } else {
+          link.classList.remove("active");
+        }
+      });
+    });
+  }
 
   // define routes
   app.route({
@@ -9,6 +43,7 @@ $(document).ready(function () {
     onCreate: console.log("Created"),
     onReady: function () {
       window.scrollTo(0, 0);
+      updateActiveLink("home");
     },
   });
 
@@ -17,6 +52,7 @@ $(document).ready(function () {
     onCreate: console.log("Created"),
     onReady: function () {
       window.scrollTo(0, 0);
+      updateActiveLink("questions");
     },
   });
 
@@ -25,6 +61,7 @@ $(document).ready(function () {
     onCreate: console.log("Created"),
     onReady: function () {
       window.scrollTo(0, 0);
+      updateActiveLink("booking");
     },
   });
 
@@ -33,6 +70,7 @@ $(document).ready(function () {
     onCreate: console.log("Created"),
     onReady: function () {
       window.scrollTo(0, 0);
+      updateActiveLink("portfolio");
     },
   });
 
@@ -41,6 +79,7 @@ $(document).ready(function () {
     onCreate: console.log("Created"),
     onReady: function () {
       window.scrollTo(0, 0);
+      updateActiveLink("comments");
     },
   });
 
